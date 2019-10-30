18c17e9ee5bb96b0800f836999dd376967c3ad33
chungcha <charlieschung@gmail.com>
2019-10-29 23:24:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", createToDo)
 });
+
+
+
+function createToDo(event){
+  event.preventDefault()
+  let description = event.target[0].value
+  let item = document.createElement("li")
+  let button  = document.createElement("button")
+  let list = document.getElementById("tasks")
+  list.appendChild(item).innerText = description
+  item.append(button)
+  document.getElementById("create-task-form").reset();
+  let buttonsArr = document.querySelectorAll("button")
+  buttonsArr.forEach(function(button){
+    button.addEventListener("click", removeItem)  //is there a scope to this?
+  })
+}
+
+
+
+function removeItem(event){
+  event.target.parentElement.remove()
+}
+
